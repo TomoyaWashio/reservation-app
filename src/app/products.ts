a20d376257a57d26b1204ef281ce578aff39bee9
TomoyaWashio <148823993+TomoyaWashio@users.noreply.github.com>
2023-12-08 11:18:26 +0900

    Angularフロントエンド側作成

---
+++ src/app/products.ts
@@ -0,0 +1,65 @@
+export const products = [
+    {
+      coverImage:'./assets/img/phone-cover.jpg',
+      id: 1,
+      name: 'Phone XL',
+      price: 799,
+      stock: 50,
+      description: 'A large phone with one of the best screens',
+      heading1:'サンプルテキスト１',
+      heading2:'サンプルテキスト２',
+      heading3:'サンプルテキスト３',
+      headingtext1:'サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト',
+      headingtext2:'テキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト',
+      headingtext3:'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+    },
+    {
+      coverImage:'./assets/img/phone-cover.jpg',
+      id: 2,
+      name: 'Phone Mini',
+      price: 699,
+      stock: 12,
+      description: 'A great phone with one of the best cameras',
+      heading1:'サンプルテキスト１',
+      heading2:'サンプルテキスト２',
+      heading3:'サンプルテキスト３',
+      headingtext1:'サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト',
+      headingtext2:'テキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト',
+      headingtext3:'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+
+    },
+    {
+      coverImage:'./assets/img/phone-cover.jpg',
+      id: 3,
+      name: 'Phone Standard',
+      price: 299,
+      stock: 30,
+      description: '',
+      heading1:'サンプルテキスト１',
+      heading2:'サンプルテキスト２',
+      heading3:'サンプルテキスト３',
+      headingtext1:'サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト',
+      headingtext2:'テキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト',
+      headingtext3:'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+
+    },
+    {
+      coverImage:'./assets/img/phone-cover.jpg',
+      id: 4,
+      name: 'Phone Standard',
+      price: 299,
+      stock: 30,
+      description: '',
+      heading1:'サンプルテキスト１',
+      heading2:'サンプルテキスト２',
+      heading3:'サンプルテキスト３',
+      headingtext1:'サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト',
+      headingtext2:'テキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト',
+      headingtext3:'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+
+    },
+  
+  ];
+  
+  
+  
